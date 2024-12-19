bde46180e35458df2f980cae7bce55dc34dabdfc
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-12-19 17:55:13 -0500

    Release: 1.2.6

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="batchfetch",
-    version="1.2.5",
+    version="1.2.6",
     packages=find_packages(),
     description="Efficiently clone and pull multiple Git repositories.",
     license="GPLv3",
